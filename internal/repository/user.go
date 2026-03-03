37e2b605a42f4ef65f4ed92212b4421af7e3da26
Vincentius <vincentius85@gmail.com>
2026-03-03 14:29:21 +0700

    feat: adding create user, get user by email, update user, and delete user

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -14,6 +14,32 @@ func NewUserRepository(db *[]models.User) *UserRepository{
 	}
 }
 
+//Get all user
 func (r *UserRepository) GetAll() *[]models.User {
 	return r.db
+}
+
+//Create a user
+func (r *UserRepository) Create(user models.User) {
+	*r.db = append(*r.db, user)
+}
+
+//Get User by Email
+func (r *UserRepository) GetByEmail(email string) (*models.User, int){
+	for i, u := range *r.db {
+		if u.Email == email {
+			return &(*r.db)[i], i
+		}
+	}
+	return nil, -1
+}
+
+//Update User
+func (r *UserRepository) Update(index int, updatedUser models.User){
+	(*r.db)[index] = updatedUser
+}
+
+//Delete User
+func (r *UserRepository) Delete(index int){
+	*r.db = append((*r.db)[:index], (*r.db)[index+1:]...)
 }
