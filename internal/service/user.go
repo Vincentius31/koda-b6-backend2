4c93afdb0fc1e64288d42e7af4a9e33485f3fa4a
Vincentius <vincentius85@gmail.com>
2026-03-03 14:31:11 +0700

    feat: adding services for create user, get user by email, update user, and delete user

--- internal/service/user.go
+++ internal/service/user.go
@@ -15,6 +15,44 @@ func NewUserService(repo *repository.UserRepository) *UserService {
 	}
 }
 
+// Get all User
 func (s *UserService) GetAll() []models.User {
 	return *s.repo.GetAll()
+}
+
+// Get User by Email
+func (s *UserService) GetByEmail(email string) *models.User{
+	user, _ := s.repo.GetByEmail(email)
+	return user
+}
+
+// Create User
+func (s *UserService) Create(req models.CreateUserRequest){
+	newUser := models.User{
+		Email: req.Email,
+		Password: req.Password,
+	}
+	s.repo.Create(newUser)
+}
+
+// Update User
+func (s *UserService) Update(email string, req models.UpdateUserRequest)bool{
+	user, index := s.repo.GetByEmail(email)
+	if user == nil {
+		return false
+	}
+
+	user.Password = req.Password
+	s.repo.Update(index, *user)
+	return true
+}
+
+// Delete User
+func (s *UserService) Delete(email string)bool{
+	_, index := s.repo.GetByEmail(email)
+	if index == -1 {
+		return false
+	}
+	s.repo.Delete(index)
+	return true
 }
