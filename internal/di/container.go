bae8ce81f479a5fe1dc74cf144cc3ba7cb4c8a27
Vincentius <vincentius85@gmail.com>
2026-04-13 11:30:07 +0700

    refactor: update container to inject DB and Cache

--- internal/di/container.go
+++ internal/di/container.go
@@ -2,51 +2,51 @@ package di
 
 import (
 	"koda-b6-backend2/internal/handlers"
-	"koda-b6-backend2/internal/models"
 	"koda-b6-backend2/internal/repository"
 	"koda-b6-backend2/internal/service"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
 )
 
 type Container struct {
-	user *[]models.User
-	userRepo *repository.UserRepository
+	db    *pgxpool.Pool
+	cache *redis.Client
+
+	userRepo    *repository.UserRepository
 	userService *service.UserService
 	userHandler *handlers.UserHandler
 
-	product *[]models.Product
-	productRepo *repository.ProductRepository
+	productRepo    *repository.ProductRepository
 	productService *service.ProductService
 	productHandler *handlers.ProductHandler
 }
 
-func NewContainer() *Container{
-	var DataUser []models.User
-	var DataProduct []models.Product
-
+func NewContainer(db *pgxpool.Pool, cache *redis.Client) *Container {
 	container := Container{
-		user: &DataUser,
-		product: &DataProduct,
+		db:    db,
+		cache: cache,
 	}
-
-	container.initDependencies() 
-
+	container.initDependencies()
 	return &container
 }
 
 func (c *Container) initDependencies() {
-	c.userRepo = repository.NewUserRepository(c.user)
-	c.userService = service.NewUserService(c.userRepo)
+	// User
+	c.userRepo = repository.NewUserRepository(c.db)
+	c.userService = service.NewUserService(c.userRepo, c.cache)
 	c.userHandler = handlers.NewUserHandler(c.userService)
 
-	c.productRepo = repository.NewProductRepository(c.product)
-	c.productService = service.NewProductService(c.productRepo)
+	// Product
+	c.productRepo = repository.NewProductRepository(c.db)
+	c.productService = service.NewProductService(c.productRepo, c.cache)
 	c.productHandler = handlers.NewProductHandler(c.productService)
 }
 
-func (c *Container) UserHandler() *handlers.UserHandler{
+func (c *Container) UserHandler() *handlers.UserHandler {
 	return c.userHandler
 }
 
-func (c *Container) ProductHandler() *handlers.ProductHandler{
+func (c *Container) ProductHandler() *handlers.ProductHandler {
 	return c.productHandler
 }
